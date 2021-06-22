585642d0dfd3164d60030dd6f9561b71159f38e0
Aleks Zosuls <azosuls@bu.edu>
2021-06-22 06:13:08 -0400

    functionalized the main to add new functions

--- DACFuncGen/DACFuncGen.ino
+++ DACFuncGen/DACFuncGen.ino
@@ -1,31 +1,83 @@
 /*
  * Simple binary counter for the DAC
  * Aleks Zosuls 2021
+ * 
+ *    //delayMicroseconds(151);  //hand adjusted delay that sets sample update rate
+   // C 440 Hz is approximately 151 uS.
  */
+#define ptsPerCycle 7
+byte signal1[ptsPerCycle] = {8, 15, 13, 7, 8, 2, 0};
+byte signal2[ptsPerCycle] = {8, 15, 13, 7, 8, 2, 0};
 byte rampCount = 8;
 
+void playArb(byte signa[], int cycles);
+void playRamp(void);
+
 void setup() {
-Serial.begin(1000000);
+Serial.begin(9600);
 DDRD = DDRD | B11110000;    //set digital pins D4-D7 as digital outputs
 pinMode(13, OUTPUT);
 digitalWrite(13, HIGH);
 PORTD = B11110000 & (rampCount << 4); 
 delay(2000);
 digitalWrite(13, LOW);
+Serial.println("Arduino signal Generator for EK307");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-      if (rampCount == 0)
-    { rampCount = 16;
-    }
-     PORTD = B11110000 & (rampCount << 4); 
-     //place the lower nibble in the upper nibble of port D using a bit shift op
-    delay(10); 
-   //delayMicroseconds(151);  //hand adjusted delay that sets sample update rate
-   // C 440 Hz is approximately 151 uS.
-    -- rampCount;  // this is a decrement because or circuit is an inverter
+  int controlByte;
+  // put your main code here, to run FOR EVER!!
+  if (Serial.available() > 0) {
+      controlByte = Serial.read(); 
+      controlByte = toupper(controlByte); //make it upper case
+      //UI check for commands then 
+      switch (controlByte) {
+        case '1':
+          Serial.println("Signal 1");
+          delay(100);
+          playArb(signal1, 1000);
+          break;
+  
+         case 'R':
+           Serial.println("Ramp generator!");
+           delay(100);
+           playRamp();
+           break;
+  
+         case '2':
+           Serial.println("Signal 2");
+           playArb(signal2, 1000);
+           break;
+      } //end of switchcase
+  } //end of if Serial.avail...  
+Serial.println("input 1,2,R for a signal");
+} //end of looop
 
-    //rampCount = rampCount % 256;
-    //Serial.println(rampCount);
-}
+void playArb(byte signa[], int cycles){
+  int circIndex = 0;
+  int endPoint = cycles * ptsPerCycle;
+  int point = 0;
+  while(point <= endPoint){
+    PORTD = B11110000 & (signa[circIndex] << 4); 
+    delayMicroseconds(10);
+    ++circIndex;
+    circIndex = circIndex % (ptsPerCycle-1);
+    ++ point;
+  } //end of while(point...
+} //end of playArb
+
+void playRamp(void) {
+  byte rampCount = 8;
+  int endPoint = 8 * 10;
+  int point = 0;
+    while(point <= endPoint){
+      if (rampCount == 0)
+      { rampCount = 16;
+      }
+      PORTD = B11110000 & (rampCount << 4); 
+     //place the lower nibble in the upper nibble of port D using a bit shift op
+      delay(10); 
+      -- rampCount;  // this is a decrement because or circuit is an inverter
+      ++ point;
+    } //end of while(point...
+} //end of playRamp
---
+++ fourBitWaveformSynth.m
@@ -0,0 +1,32 @@
+% Make a simple synthesized signal for microcontroller playback into a
+% simple 4 bit parallel DAC.
+% Copyright 2021 Aleks Zosuls
+
+%sampleRate = 2000;
+ptsPerCycle = 7;
+A1 = 1;
+F1 = 110;
+A2 = .5;
+F2 = 2* F1;
+A3 = .2;
+F3 = 2* F2;
+T = (0:ptsPerCycle-1)./(ptsPerCycle*F1);
+%T = (0:100-1)./(ptsPerCycle*F1);
+s1 = A1* sin(2*pi*F1*T);
+s2 = A2* sin(2*pi*F2*T);
+s3 = A3* sin(2*pi*F3*T);
+
+supahhPos = s1+s2+s3;
+spNorm = supahhPos/max(supahhPos);
+spShift = 15* (0.5*spNorm + 0.5);
+quantz = round(spShift);
+figure
+plot(T,quantz)
+
+
+%%
+
+
+%use quantizer to make four bit version
+%q = quantizer('mode', 'ufixed', 'overflowmode', 'saturate', 'format', [5 4]);
+%output2 = quantize(q, spShift);
